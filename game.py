49d012d6f96596e02f096ada1257f71a0714a11a
PiKwaii93 <78363268+PiKwaii93@users.noreply.github.com>
2021-05-03 11:58:29 +0200

    Add files via upload

---
+++ game.py
@@ -0,0 +1,13 @@
+from player import Player
+from Mot import Mot
+from Phrase import Phrase
+
+class GAME :
+
+    def __init__(self) :
+        self.joueur1 = Player()
+        self.joueur2 = Player()
+        self.Combat()
+
+
+
