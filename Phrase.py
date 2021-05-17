4df9d5915dbce67f2b54f99e1876f1c37d3d84b0
Sanjayan-sri <78940444+Sanjayan-sri@users.noreply.github.com>
2021-05-17 03:25:01 +0200

    Update Phrase.py

--- Phrase.py
+++ Phrase.py
@@ -3,17 +3,17 @@ class Phrase :
     def __init__(self, tour):
         self.tour = tour
         if tour=="1" :
-            self.phrase = "Ta mere est tellement grosse..."
+            self.phrase = "Ta mere est tellement..."
         if tour=="2" :
-            self.phrase = "Le matin ideale pour toi ? Cafe, douche et ..."
+            self.phrase = "Ton père est tellement bête..."
         if tour=="3" :
-            self.phrase = ""
+            self.phrase = "Ton frère est tellement"
         if tour=="4" :
-            self.phrase = ""
+            self.phrase = "Ta soeur"
         if tour=="5" :
-            self.phrase = ""
+            self.phrase = "Quand je te vois,"
         if tour=="6" :
-            self.phrase = ""
+            self.phrase = "t'es tellement grade"
 
         
         
